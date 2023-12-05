8d17dab99cecc5469455f1900c3f8e67430ec5fa
aurora <aurora.kuusisto@netikka.fi>
2023-12-05 18:12:39 +0200

    syötteen kysyminen ja tiedoston luku lisätty

---
+++ projekti.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def weather(day, month, year):
+    df_weather = pd.read_csv("saa_seinajoki.csv")
+
+
+def get_day_month_year(date):
+    day = ""
+    month = ""
+    year = ""
+    for char in range(len(date)):
+        # ota talteen
+
+
+def main():
+    date = input("Choose a date (dd.mm.yyyy)")
+    # virhetarkistukset
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
